b17ba2405d91ab52f2c97116731d7d0d591deea2
michalkrajnak <michal.krajnak0@gmail.com>
2015-11-21 17:28:48 +0100

    Create DopravnyUzol.cpp

---
+++ DopravnyUzol.cpp
@@ -0,0 +1,20 @@
+#include "simlib.h"
+// pouzite kvoli std::atol
+#include <cstdlib>
+
+// pouzite pre std::string na meno suboru
+#include <string>
+
+class Vlak : public Process
+{
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
+	// premenne na ukladanie argumentov prikazoveho riadka
+	std::string filename = "DopravnyUzol.out";
+	
+}
